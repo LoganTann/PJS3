706ff9e4abef6ba712c8663cb124943dd1be8f34
LoganTann <logon313@hotmail.fr>
2021-10-07 20:38:55 +0200

    refactorisation

--- functions/meteo.js
+++ functions/meteo.js
@@ -1,46 +1,37 @@
-const { JSDOM } = require('jsdom');
-const fetch = require('node-fetch');
-const cowsay = require('cowsay');
+const { JSDOM } = require('jsdom')
+const fetch = require('node-fetch')
+const cowsay = require('cowsay')
 
-const API_ENDPOINT = 'https://services.data.shom.fr/hdm/vignette/petite/BREHAT_MEN_JOLIGUET?locale=fr';
+const API_ENDPOINT = 'https://services.data.shom.fr/hdm/vignette/petite/BREHAT_MEN_JOLIGUET?locale=fr'
 
 exports.handler = async (_event, _context) => {
   try {
-    const response = await fetch(API_ENDPOINT);
-    let text = await response.text();
-    
-    text = text.split("document.open();")[1];
+    const response = await fetch(API_ENDPOINT)
+    const text = await response.text()
+      .split('document.open();')[1]
+      .replace(/(\\)|(ifrm\.document\.(write|open|close)\(('*))|('*)\);/gmi, '')
 
-    const regex = /(\\)|(ifrm\.document\.(write|open|close)\(('*))|('*)\);/gmi;
+    const dom = new JSDOM(text)
+    const heures = dom.window.document.querySelectorAll('td')
 
-    text = text.replace(regex, '');
-
-    const dom = new JSDOM(text);
-    const heures = dom.window.document.querySelectorAll("td");
-
-    const out = [];
+    const out = []
     for (const heure of heures) {
-      out.push(heure.textContent.replace(/\\n|[^A-Za-z0-9\:\-\.]/gmi, ""));
+      out.push(heure.textContent.replace(/\\n|[^A-Za-z0-9:\-.]/gmi, ''))
     }
-
-    // const heure = heures[1].innerHTML;
-    // const marree = heures[3].innerHTML.includes("--") ? "basse" : "haute";
-
     const say = cowsay.say({
-      cow: "awesome-face",
-      text: `À Bréhat, la prochaine marrée ${out[3].includes("-") ? 'basse':'haute'} sera à ${out[1]}`
-    }).split("\n");
+      cow: 'awesome-face',
+      text: `À Bréhat, la prochaine marrée ${out[3].includes('-') ? 'basse' : 'haute'} sera à ${out[1]}`
+    }).split('\n')
+
     return {
-      statusCode: 200, 
-      headers: {
-        "Content-Type": "application/json"
-      },
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ out, cow: say })
-    };
+    }
   } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify({ error: `failed fetching data : ${error}` })
-    };
+    }
   }
-};
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header-homepage class="header-homepage"/>
+    <header-homepage class="header-homepage" />
     <content-homepage class="content-homepage">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolores expedita modi nesciunt! Dolores, ea, veniam? Animi at, error non numquam perspiciatis praesentium quae quaerat sint. Adipisci eligendi fugit voluptatibus.
     </content-homepage>
@@ -8,10 +8,10 @@
 </template>
 <script>
 import HeaderHomepage from '~/components/header-homepage'
-import Container from '~/components/container'
 import ContentHomepage from '~/components/content-homepage'
+
 export default {
-  components: { ContentHomepage, HeaderHomepage, Container },
+  components: { ContentHomepage, HeaderHomepage },
   data () {
     return {
     }
@@ -21,11 +21,5 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-/*.index-page {
-  display: flex;
-  flex-direction: column;
-}
-.content-homepage {
-  flex-grow: 1;
-}*/
+
 </style>

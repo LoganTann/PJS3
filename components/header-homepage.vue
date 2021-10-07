7c7eec15655bb44c15a70f6360e0a2978f7d960d
LoganTann <logon313@hotmail.fr>
2021-10-07 21:12:41 +0200

    ajout vidéo page accueil + deps lambdas

--- components/header-homepage.vue
+++ components/header-homepage.vue
@@ -1,11 +1,22 @@
 <template>
   <header>
-    <div class="texte">
-      <h1>Coucou ceci est une catchphrase</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <div class="carre">
-      carré
+    <video
+      id="bgvid"
+      playsinline
+      autoplay
+      muted
+      loop
+    >
+      <source src="/brehat.mp4" type="video/mp4">
+    </video>
+    <div class="header-content">
+      <div class="texte">
+        <h1>Coucou ceci est une catchphrase</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div class="meteo">
+        carré
+      </div>
     </div>
   </header>
 </template>
@@ -18,7 +29,24 @@ export default {
 
 <style scoped>
 header {
-  padding: 10em 4em;
+  position: relative;
+  display: flex;
+}
+video {
+  width: 100%;
+  height: auto;
+  position: relative;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+.header-content {
+  z-index: 2;
+  position: absolute;
+  top: 0; left: 0;
+  width: 100%; height: 100%;
+  background-color: rgba(0,0,0,0.3);
+  color: white;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -26,5 +54,9 @@ header {
 
 .texte {
   width: 30em;
+  padding: 0 4em;
+}
+.meteo {
+  padding-right: 4em;
 }
 </style>

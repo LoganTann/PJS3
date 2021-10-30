4613134132ba4104b539ad40dcad9eacc591fb42
LoganTann <logon313@hotmail.fr>
2021-10-30 14:32:59 +0200

    refactor: refactor meteo.js

--- functions/meteo.js
+++ functions/meteo.js
@@ -3,34 +3,44 @@ const fetch = require('node-fetch')
 const cowsay = require('cowsay')
 
 const { WEATHERSTACK_API_KEY } = process.env
-const MAREE_ENDPOINT = 'https://services.data.shom.fr/hdm/vignette/petite/BREHAT_MEN_JOLIGUET?locale=fr'
+const MAREE_ENDPOINT =
+  'https://services.data.shom.fr/hdm/vignette/petite/BREHAT_MEN_JOLIGUET?locale=fr'
 const METEO_ENDPOINT = `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_API_KEY}&query=48.517,-2.75`
+
+async function mareeJson () {
+  const maree = await fetch(MAREE_ENDPOINT)
+  const scrappedHTML = (await maree.text())
+    .split('document.open();')[1]
+    .replace(/(\\)|(ifrm\.document\.(write|open|close)\(('*))|('*)\);/gim, '')
+  const dom = new JSDOM(scrappedHTML)
+  const heures = dom.window.document.querySelectorAll('td')
+
+  const out = []
+  for (const heure of heures) {
+    out.push(heure.textContent.replace(/\\n|[^A-Za-z0-9:\-.]/gim, ''))
+  }
+  return out
+}
+
+async function meteoJson () {
+  const meteo = await fetch(METEO_ENDPOINT)
+  return await meteo.json().current
+}
+
+function createCow (meteo) {
+  return cowsay
+    .say({
+      cow: 'awesome-face',
+      text: `À Bréhat, la prochaine marrée ${
+        meteo[3].includes('-') ? 'basse' : 'haute'
+      } sera à ${meteo[1]}`
+    })
+    .split('\n')
+}
 
 exports.handler = async (_event, _context) => {
   try {
-    const maree = await fetch(MAREE_ENDPOINT)
-    const meteo = await fetch(METEO_ENDPOINT)
-
-    const text = (await maree.text())
-      .split('document.open();')[1]
-      .replace(/(\\)|(ifrm\.document\.(write|open|close)\(('*))|('*)\);/gmi, '')
-
-    const meteoJson = (await meteo.json()).current
-
-    const dom = new JSDOM(text)
-    const heures = dom.window.document.querySelectorAll('td')
-
-    const out = []
-    for (const heure of heures) {
-      out.push(heure.textContent.replace(/\\n|[^A-Za-z0-9:\-.]/gmi, ''))
-    }
-    const cow = cowsay.say({
-      cow: 'awesome-face',
-      text: `À Bréhat, la prochaine marrée ${out[3].includes('-') ? 'basse' : 'haute'} sera à ${out[1]}`
-    }).split('\n')
-
     const headers = { 'Content-Type': 'application/json' }
-
     // L'api sera accessible publiquement seulement avant décembre
     if (new Date().getTime() < new Date('2021-12-31').getTime()) {
       headers['Access-Control-Allow-Origin'] = '*'
@@ -38,10 +48,15 @@ exports.handler = async (_event, _context) => {
       headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTION'
     }
 
+    // construction du body de la requête
+    const meteo = await meteoJson()
+    const maree = await mareeJson()
+    const cow = createCow(meteo)
+
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify({ cow, out, meteoJson })
+      body: JSON.stringify({ cow, maree, meteo })
     }
   } catch (error) {
     return {
